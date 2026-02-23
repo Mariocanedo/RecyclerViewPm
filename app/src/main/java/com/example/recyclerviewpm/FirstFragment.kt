e5d1f9effabfc08ff4ebb544cedbe1c9a8d4163a
mc <Mario_cane.do@hotmail.com>
2026-02-23 16:16:00 -0300

    implementación adapter, falta seleccionar palabra e ir a otra pantalla y agregar palabras con el button

--- app/src/main/java/com/example/recyclerviewpm/FirstFragment.kt
+++ app/src/main/java/com/example/recyclerviewpm/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.example.recyclerviewpm
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerviewpm.databinding.FragmentFirstBinding
 
 /**
@@ -39,6 +41,23 @@ class FirstFragment : Fragment() {
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+
+        // para probar las palabras
+        Log.d("Listado", setData().toString())
+
+        //1 Instanciar el adapter y le pasamos los datos con las palabras
+
+        val adapter = WordAdapter(setData())
+        //2 le pasamos el adapter al recyclerView
+
+        binding.Rv.setAdapter(adapter)
+
+        // paso 3 le indicamos al recyclerview como mostrar los datos
+
+        binding.Rv.setLayoutManager(LinearLayoutManager(getContext()))
+        binding.Rv.setHasFixedSize(true)
+
     }
 
 
--- app/src/main/java/com/example/recyclerviewpm/WordAdapter.kt
+++ app/src/main/java/com/example/recyclerviewpm/WordAdapter.kt
@@ -1,4 +1,76 @@
 package com.example.recyclerviewpm
 
-class WordAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewpm.databinding.WordItemListBinding
+
+/*****************4.- EXTIENDO LA CLASE WORDADATER de Recyclerview.Adapter CONTIENE LA CLASE INTERNA WORDVIEWHOLER */
+// 4 -implementar los métos de adapter OnCreateViewHolder, OnBindViewHolder, getItemCount()
+class WordAdapter
+/***************** Paso 2.Representación de los datos */
+    (private val mwordList: MutableList<String?> ): RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+
+
+
+
+
+
+
+
+    //6 ESTE METODO INICIALIZA LAS VISTAS LAS INFLA
+    // EN EL CONSTRUCTOR RECIBE UN OBJETO LE PASAMOS BINDING
+    // INSTANCIAMOS VIEWBINDING PARA RETONAR NUESTRO VIEWHOLDER CON ESA DEPENDENCIA
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+
+        val mBinding = WordItemListBinding.inflate(LayoutInflater.from(parent.getContext()),
+        parent, false)
+        return WordViewHolder(mBinding)
+
+    }
+
+
+
+
+
+    // paso 7 ESTE  MÉTODO DIBUJA LAS VISTAS
+    // VA POSICIONANDO CADA ELEMENTO DEL RV
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int
+    ) {
+
+        val elemento =mwordList.get(position)
+        holder.textView.setText(elemento)
+
+    }
+
+
+
+
+    override fun getItemCount(): Int {
+
+        return mwordList.size
+    }
+
+
+    /*****************1. Paso Crear clase interna que se llama ViewHolder */
+    // INICIALIZAMOs TEXVIEW QUE ES DENTRO DEL XML  Y LLAMAMOS MBINDING QUE ES DE LA CLASE XML QUE CONTIENE TEXVIEW
+    // NOS PEDIRA IMPLEMENTAR SU CONSTRUCTOR DE LA SUPER CLASE
+    // CAMBIAMOS EL ELEMENTO ANTERIOR Y REFERENCIAMOS A LA CLASE BINDING QUE REPRESENTA NUESTRO LAYOUT WORD ITEM LIST
+
+
+    inner class WordViewHolder(mBinding: WordItemListBinding) :
+        RecyclerView.ViewHolder(mBinding.getRoot()) {
+
+        val textView: TextView
+
+        init {
+
+            textView = mBinding.textView
+        }
+    }
+
+
+
 }
